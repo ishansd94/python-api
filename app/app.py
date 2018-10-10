214c28f220d548d11a7d956e1cb08e60e5045141
Chamara Singhabahu <mails.chamara@gmail.com>
2018-10-10 09:20:52 +0530

    Create endpoint to get info

--- app/app.py
+++ app/app.py
@@ -1,10 +1,22 @@
 from flask import Flask
+import json
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     return "Hello, World!"
+
+
+@app.route('/info')
+def info():
+    file = open('../VERSION')
+    return json.dumps({
+        'version': file.read().strip(),
+        'name': 'python-api'
+    })
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=80)
